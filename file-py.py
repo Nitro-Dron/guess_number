5345987906f4b3021fee9018dc5c13ec6e7d7a52
Denis Danilov <denis07_07@inbox.ru>
2024-07-10 11:44:26 +0200

    s

--- file-py.py
+++ file-py.py
@@ -1,4 +1,24 @@
-for i in range(8):
-    for j in range(i + 1):
-        print('*', end='')
-    print()
+class Employee:
+    vacation_days = 28
+
+    def __init__(self, first_name, second_name, gender):
+        self.first_name = first_name
+        self.second_name = second_name
+        self.gender = gender
+        self.remaining_vacation_days = Employee.vacation_days
+        # Сюда добавьте новый атрибут remaining_vacation_days
+
+    # Сюда добавьте методы consume_vacation и get_vacation_details.
+    def consume_vacation(self, days):
+        self.remaining_vacation_days -= days
+
+    def get_vacation_details(self):
+        return f'Остаток отпускных дней: {self.remaining_vacation_days}.'
+# Пример использования класса:
+# employee = Employee('Роберт', 'Крузо', 'м')
+# employee.consume_vacation(7)
+
+
+employee1 = Employee('Роберт', 'Крузо', 'м')
+employee1.consume_vacation(7)
+print(employee1.get_vacation_details())
